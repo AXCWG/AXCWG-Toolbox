bc692e885bc567d8a95485a4d94e7a0aef3032b6
AXCWG <xiey0@qq.com>
2024-11-09 10:43:29 +0800

    everything

---
+++ src/ZenType.js
@@ -0,0 +1,23 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import { useState } from 'react';
+import './App.css';
+import './ZenType.css';
+
+function ZenType() {
+    const [textThings, setTextThings] = useState("");
+    return (
+        <>
+            <textarea style={{ width: "100vw", }} value={textThings} onChange={(e) => { setTextThings(e.currentTarget.value) }}></textarea>
+            <div id='sub'>
+                <button className='btn btn-primary' style={{ borderRadius: "0px" }} onClick={() => { const url = window.URL.createObjectURL(new Blob([textThings], {type: "text/plain"}),);window.location.href = url }}>Download</button>
+                <button className='btn btn-danger' style={{ borderRadius: "0px" }}>Fuck all this.</button>
+
+            </div>
+
+        </>
+    )
+}
+
+
+export default ZenType 
