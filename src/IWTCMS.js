a522ef8bcf91c95f57b85edf73234cf37449dabb
AXCWG <xiey0@qq.com>
2024-11-11 18:26:56 +0800

    fixes

--- src/IWTCMS.js
+++ src/IWTCMS.js
@@ -1,5 +1,55 @@
-function Iwtcms(){
+import { useState } from "react"
 
+
+function Iwtcms() {
+    const [form, setForm] = useState({
+        host: "",
+        password: "",
+    })
+    const [connected, setConnected] = useState(false)
+    return (
+        <div className="container mt-5">
+            <div className="display-4">I-Want-To-Control-My-Server</div>
+            <div className="mt-3 container-fluid bg-warning rounded-3 p-3 " style={{color: "rgb(130, 40, 0)"}}>
+                <h4>Warning:</h4>
+                The IWTCMS mod is based on plain TCP Socket communication,
+                which is a protocal that mainstream browsers don't support.
+                This website uses a proxy of my own to connect to the server.
+                If you have doubt about the proxy or safety or all other stuff,
+                <strong> DO NOT</strong> use this site.
+            </div>
+            {
+                connected
+                    ?
+                    <div className="text-success text-center mt-2">
+                        Status: <strong>Connected. </strong>
+                    </div>
+                    :
+                    <div className="text-danger text-center mt-2">
+                        Status: <strong>Not Connected. </strong>
+                    </div>
+            }
+            <div className=" mt-2"  >
+                <div style={{ display: "flex", justifyContent: "space-between", gap: "10px" }}>
+                    <input className="form-control" type="text" placeholder="Host" value={form.host} onChange={(e) => {
+                        setForm({
+                            ...form,
+                            host: e.target.value,
+
+                        })
+                    }}></input>
+                    <input className="form-control" type="password" placeholder="Password" value={form.password} onChange={(e) => {
+                        setForm({
+                            ...form,
+                            password: e.target.value,
+                        })
+                    }}></input>
+                </div>
+
+                <button className="w-100 btn btn-primary mt-2">Connect</button>
+            </div>
+        </div>
+    )
 }
 
 export default Iwtcms
--- src/ZenType.js
+++ src/ZenType.js
@@ -11,7 +11,7 @@ function ZenType() {
             <textarea style={{ width: "100vw", }} value={textThings} onChange={(e) => { setTextThings(e.currentTarget.value) }}></textarea>
             <div id='sub'>
                 <button className='btn btn-primary' style={{ borderRadius: "0px" }} onClick={() => { const url = window.URL.createObjectURL(new Blob([textThings], {type: "text/plain"}),);window.location.href = url }}>Download</button>
-                <button className='btn btn-danger' style={{ borderRadius: "0px" }}>Fuck all this.</button>
+                <button className='btn btn-danger' style={{ borderRadius: "0px" }} onClick={()=>{setTextThings("")}}>Fuck all this.</button>
 
             </div>
 
