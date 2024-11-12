41209b769d643a6c97becbe8f0e62dedbe204510
AXCWG <xiey0@qq.com>
2024-11-12 09:15:15 +0800

    update

--- src/IWTCMS.js
+++ src/IWTCMS.js
@@ -10,7 +10,7 @@ function Iwtcms() {
         host: "",
         password: "",
     })
-    const [connected, setConnected] = useState(false)
+    const [connected, setConnected] = useState(1)
     const [res, setRes] = useState("")
     const [command, setCommand] = useState("")
     return (
@@ -24,16 +24,23 @@ function Iwtcms() {
                 If you have doubt about the proxy or safety or all other stuff,
                 <strong> DO NOT</strong> use this site.
             </div>
+            <div className="text-center ">
+                Currently only supports Non-tls connection
+            </div>
             {
-                connected
+                connected === 0
                     ?
                     <div className="text-success text-center mt-2">
                         Status: <strong>Connected. </strong>
                     </div>
-                    :
-                    <div className="text-danger text-center mt-2">
-                        Status: <strong>Not Connected. </strong>
-                    </div>
+                    : connected === 1 ?
+                        <div className="text-danger text-center mt-2">
+                            Status: <strong>Not Connected. </strong>
+                        </div>
+                        : connected === 2 ?
+                            <div className="text-center mt-2">
+                                Status: <strong>Connecting</strong>
+                            </div> : null
             }
             <div className=" mt-2"  >
                 <div style={{ display: "flex", justifyContent: "space-between", gap: "10px" }}>
@@ -52,38 +59,48 @@ function Iwtcms() {
                     }}></input>
                 </div>
                 {
-                    connected ?
+                    (connected === 0 || connected === 2) ?
                         <button className="w-100 btn btn-primary mt-2" onClick={() => {
                             ws.close();
-                            setConnected(false)
+                            setConnected(1)
                         }}>Disconnect</button>
-                        :
+                        : connected === 1 ?
 
-                        <button className="w-100 btn btn-primary mt-2" onClick={() => {
-                            ws = new WebSocket("ws://127.0.0.1:8090")
-                            ws.onopen = () => {
-                                ws.send(JSON.stringify(form))
-                            }
-                            ws.onmessage = (data) => {
-                                append = (append === undefined ? "" : append) + data.data
-                                setRes(append)
-                                if (data.data === "iwtcms_login_success\n") {
-                                    setConnected(true)
+                            <button className="w-100 btn btn-primary mt-2" onClick={() => {
+                                setConnected(2)
+                                ws = new WebSocket("wss://andyxie.cn:8090")
+                                ws.onopen = () => {
+                                    ws.send(JSON.stringify(form))
                                 }
-                                var textarea = document.getElementById("output");
+                                ws.onmessage = (data) => {
+                                    append = (append === undefined ? "" : append) + data.data
+                                    setRes(append)
+                                    try {
+                                        var parsed = JSON.parse(data.data)
+                                        append = (append === undefined ? "" : append) + parsed.message
 
-                                if (textarea.selectionStart == textarea.selectionEnd) {
-                                   setTimeout(() => {
-                                     textarea.scrollTop = textarea.scrollHeight; 
-                                   }, 100);
+                                        return
+                                    } catch {
+
+                                    }
+                                    if (data.data === "iwtcms_login_success\n") {
+                                        setConnected(0)
+                                    }
+                                    var textarea = document.getElementById("output");
+
+                                    if (textarea.selectionStart == textarea.selectionEnd) {
+                                        setTimeout(() => {
+                                            textarea.scrollTop = textarea.scrollHeight;
+                                        }, 100);
+                                    }
                                 }
-                            }
-                        }}>Connect</button>
+                            }}>Connect</button>
+                            : null
                 }
 
             </div>
             <div className="mt-2">
-                <input value={command} placeholder="Command" className="w-100 form-control " disabled={connected ? false : true} onKeyDown={(data) => {
+                <input value={command} placeholder="Command" className="w-100 form-control " disabled={connected === 1 ? true : false} onKeyDown={(data) => {
                     if (data.key === "Enter") {
                         ws.send(JSON.stringify({ type: 1, command: command }))
                         setCommand("")
