c76452cf1a883246e9c586ea84cc3d6ccac940bf
AXCWG <xiey0@qq.com>
2024-11-12 11:32:11 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, createHashRouter, RouterProvider } from 'react-router-dom';
 import ZenType from './ZenType';
 import Iwtcms from './IWTCMS';
 
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App/>
